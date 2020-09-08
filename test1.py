a0df252a3e33d814654250664cfcb516bb8d4d9b
Shon <johan.miranda@correo.usa.edu.co>
2020-09-07 19:20:52 -0500

    se eliminan ejercicios de master y se deja limpia para al finalizar ahcer merge

--- test1.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from EjerciciosPy.Ejer1 import edad
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(72, edad(0, 1998))
-if __name__ == '__main__':
-    unittest.main()
